16cff383081bc5d4cb9bf1f542c5e4ba08cc9e87
dholmes3514 <dholmes3514@gmail.com>
2025-06-09 18:06:30 +0000

    Completed exercise

--- src/app.py
+++ src/app.py
@@ -8,3 +8,35 @@ load_dotenv()
 
 
 # Spotify API credentials
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+artist_id = "3NJ94iuAmmMjbszODYT6pO"
+
+# Get the top tracks of an artist
+results = spotify.artist_top_tracks(artist_id)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
+
+tracks_df = pd.DataFrame(songs)
+
+print(tracks_df.head(3))
+
+#scatter plot of Subtronic's duration of song correlation popularity 
+plt.scatter(tracks_df['duration_min'], tracks_df['popularity'])
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Popularity')
+plt.title('Relationship between Duration and Popularity')
+plt.show()
